b0c285393ed144bb939927a2c3b8ce8d0d2ea73d
PKG-7 <pavel.k.group@gmail.com>
2022-07-29 08:32:18 +0300

    starting

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,12 @@ export default function Header() {
   return (
     <header>
         <div>
-            <span className='logo'>MyReactShop</span>
+            <span className='logo'>Beast</span>
+            <ul className="nav">
+              <li>Про нас</li>
+              <li>Контакты</li>
+              <li>Кабинет</li>
+            </ul>
         </div>
         <div className='presentation'></div>
     </header>
