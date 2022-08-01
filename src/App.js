d5fe83686dd4e8f9e0f0e86c828cfb4afd44399c
PKG-7 <pavel.k.group@gmail.com>
2022-08-01 07:38:44 +0300

    waiting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { useFetch } from "./components/useFetch";
-
 
 export default function App() {
 
-  const { data: cards, isPending, error } = useFetch(`http://localhost:8000/cards/`)
-
-
+  
   return (
+    
       <div className="wrapper">
         <Header />
-
-        <div>
-           <h2>{ cards.title }</h2>
-        </div>
-
+     
         <Footer />
       </div>
   )
